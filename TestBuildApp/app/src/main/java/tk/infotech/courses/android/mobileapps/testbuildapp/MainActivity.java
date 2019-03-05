487bb3f0628512d71b2da7992f3d0cee4b97f995
ANONYMOUS-HUMANOID-DEVELOPER <glenyufui@gmail.com>
2019-03-05 23:55:20 +0100

    Some other changes made to the project

--- TestBuildApp/app/src/main/java/tk/infotech/courses/android/mobileapps/testbuildapp/MainActivity.java
+++ TestBuildApp/app/src/main/java/tk/infotech/courses/android/mobileapps/testbuildapp/MainActivity.java
@@ -2,9 +2,6 @@ package tk.infotech.courses.android.mobileapps.testbuildapp;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
