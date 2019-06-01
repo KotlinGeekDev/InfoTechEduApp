1b82cd216f3a3ef0a802fa499e6503873d7ade46
ANONYMOUS-HUMANOID-DEVELOPER <glenyufui@gmail.com>
2019-06-01 20:45:23 +0100

    Update to Project:   Changed circular animation at the beginning of the app to a new,   circular material animation.

---
+++ TestBuildApp/app/src/main/gen/tk/infotech/courses/android/mobileapps/testbuildapp/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package tk.infotech.courses.android.mobileapps.testbuildapp;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
